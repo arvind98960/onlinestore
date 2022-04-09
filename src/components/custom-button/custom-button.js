9f6f2ae59bd9cfdfa7c50e39aa771ed8ada63077
arvind98960 <arvind98960@gmail.com>
2022-04-09 16:14:58 +0530

    Update custom-button.js

--- src/components/custom-button/custom-button.js
+++ src/components/custom-button/custom-button.js
@@ -1,9 +1,10 @@
+
 import './custom-button.styles.scss';
-const CustomButton = (children,...otherProps)=> {
-          <button className="custom-button" {...otherProps}>
-            {children}
-        </button>
-    
-}
+
+const CustomButton = ({ children, ...otherProps }) => (
+  <button className='custom-button' {...otherProps}>
+    {children}
+  </button>
+);
 
 export default CustomButton;
