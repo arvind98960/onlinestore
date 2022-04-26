13b8e591befd036f913b4953862917247ac7c7ac
arvind98960 <arvind98960@gmail.com>
2022-04-26 18:42:22 +0530

    redirectToHome

--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -1,10 +1,12 @@
+import {UserActionType} from './user.actionType'
+
 const INITAIL_STATE = {
     currentUser: null,
 }
 
 const userReducer = (state = INITAIL_STATE, action) => {
     switch (action.type) {
-        case 'SET_CURRENT_USER':
+        case UserActionType.SET_CURRENT_USER:
             return {
                 ...state,
                 currentUser: action.payload 
